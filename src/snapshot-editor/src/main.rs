190ab3d344c802cae15c2a188b0214fed8ff9b44
Tomas Srnka <tomas.srnka@gmail.com>
2025-10-14 12:23:46 +0200

    CPU TSC fix

--- src/snapshot-editor/src/main.rs
+++ src/snapshot-editor/src/main.rs
@@ -7,22 +7,26 @@ mod edit_memory;
 #[cfg(target_arch = "aarch64")]
 mod edit_vmstate;
 mod info;
+mod tsc;
 mod utils;
 
 use edit_memory::{edit_memory_command, EditMemoryError, EditMemorySubCommand};
 #[cfg(target_arch = "aarch64")]
 use edit_vmstate::{edit_vmstate_command, EditVmStateError, EditVmStateSubCommand};
 use info::{info_vmstate_command, InfoVmStateError, InfoVmStateSubCommand};
+use tsc::{tsc_command, TscCommandError, TscSubCommand};
 
 #[derive(Debug, thiserror::Error, displaydoc::Display)]
 enum SnapEditorError {
     /// Error during editing memory file: {0}
     EditMemory(#[from] EditMemoryError),
-    #[cfg(target_arch = "aarch64")]
     /// Error during editing vmstate file: {0}
+    #[cfg(target_arch = "aarch64")]
     EditVmState(#[from] EditVmStateError),
     /// Error during getting info from a vmstate file: {0}
     InfoVmState(#[from] InfoVmStateError),
+    /// Error during updating TSC metadata: {0}
+    EditTsc(#[from] TscCommandError),
 }
 
 #[derive(Debug, Parser)]
@@ -36,11 +40,13 @@ struct Cli {
 enum Command {
     #[command(subcommand)]
     EditMemory(EditMemorySubCommand),
-    #[cfg(target_arch = "aarch64")]
     #[command(subcommand)]
+    #[cfg(target_arch = "aarch64")]
     EditVmstate(EditVmStateSubCommand),
     #[command(subcommand)]
     InfoVmstate(InfoVmStateSubCommand),
+    #[command(subcommand)]
+    Tsc(TscSubCommand),
 }
 
 fn main_exec() -> Result<(), SnapEditorError> {
@@ -51,6 +57,7 @@ fn main_exec() -> Result<(), SnapEditorError> {
         #[cfg(target_arch = "aarch64")]
         Command::EditVmstate(command) => edit_vmstate_command(command)?,
         Command::InfoVmstate(command) => info_vmstate_command(command)?,
+        Command::Tsc(command) => tsc_command(command)?,
     }
 
     Ok(())
